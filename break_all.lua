c1ba622016553bafc40dffd5f611ddeed99c8648
deixman1 <deix@yandex.ru>
2021-01-13 03:05:13 +0300

    Update break_all.lua

--- break_all.lua
+++ break_all.lua
@@ -143,7 +143,7 @@ function home() -- переход к начальной точке и сброс
     pos_backup.z = pos.z
     dir_backup = dir
     print('отправляюсь домой')
-    go(0, pos.y-2, 0)
+    go(pos.y-2, 0, 0)
     go(0, 0, 0)
     print('прибыл домой')
     position_at_home = true
@@ -151,8 +151,8 @@ end
 
 function return_to_work() -- переход к начальной точке и сброс лута
     print('возврат к работе')
-    go(pos_backup.x, pos_backup.y-2, pos_backup.z)
-    go(pos_backup.x, pos_backup.y, pos_backup.z)
+    go(pos_backup.y-2, pos_backup.x, pos_backup.z)
+    go(pos_backup.y, pos_backup.x, pos_backup.z)
     smart_turn(dir_backup)
     print('прибыл на работу')
     position_at_home = false
